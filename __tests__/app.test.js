a8cd3144509793d27bf2d4049087170d4a622347
Richard <wisemanrick@hotmail.co.uk>
2023-02-23 17:10:45 +0000

    Endpoint created and 1st test passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -405,6 +405,22 @@ test("Query with wrong format (Not Number) ", ()=>{
         })
     })
 })
+describe.only("GET /api/users", () =>{
+    test("returns all users", () => {
+        return request(app)
+        .get("/api/users")
+        .expect(200)
+        .then(({ body }) => {
+            const { users } = body
+            expect(users).toHaveLength(4)
+            users.forEach(user => {
+                expect(user).toHaveProperty("username", expect.any(String))
+                expect(user).toHaveProperty("name", expect.any(String))
+                expect(user).toHaveProperty("avatar_url", expect.any(String))
+            })
+        })
+    }) 
+})
 
 
 
